5f64c7d380a5a44a636cb69ea93dd129ee6126cc
KAIC <aicertificationcenter@gmail.com>
2026-04-04 22:20:55 +0900

    fix: revert to absolute standard prisma config with library engine

--- api/lib/prisma.js
+++ api/lib/prisma.js
@@ -1,17 +1,10 @@
 import { PrismaClient } from '@prisma/client';
-import { Pool } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
 
 const globalForPrisma = global;
-
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
 
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    adapter,
     log: ['query'],
   });
 
